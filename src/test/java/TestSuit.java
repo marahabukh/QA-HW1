f5069d7565973610bb6bb6a70c8837b2409db6eb
marahabukh <marahabukh48@gmail.com>
2025-12-06 14:27:01 +0200

    done

---
+++ src/test/java/TestSuit.java
@@ -0,0 +1,9 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        ProductStockTest.class
+})
+public class TestSuit {
+}
